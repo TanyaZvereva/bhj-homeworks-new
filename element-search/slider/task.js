119a7fc42f4a8d36b37b20d6e812eb098776b7ae
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2020-08-05 15:15:45 +0300

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,26 +1,16 @@
-const prev = document.querySelector(".slider__arrow_prev")
-const next = document.querySelector(".slider__arrow_next")
-const sliderItems = document.querySelector(".slider__items")
-let sliderNumber = 0
-let sliderLength = sliderItems.children.length
+const prev = document.querySelector(".slider__arrow_prev");
+const next = document.querySelector(".slider__arrow_next");
+const sliderItems = document.querySelector(".slider__items");
+let sliderNumber = 0;
+let sliderLength = sliderItems.children.length;
 
 next.onclick = () => {
-    sliderItems.children[sliderNumber].classList.remove("slider__item_active")
-    if (sliderNumber === sliderLength - 1) {
-        sliderNumber = 0
-    } else {
-        sliderNumber++
-    }
-
-    sliderItems.children[sliderNumber].classList.add("slider__item_active")
-}
+    sliderItems.children[sliderNumber].classList.remove("slider__item_active");
+    (sliderNumber === sliderLength - 1) ? sliderNumber = 0: sliderNumber++;
+    sliderItems.children[sliderNumber].classList.add("slider__item_active");
+};
 prev.onclick = () => {
-    sliderItems.children[sliderNumber].classList.remove("slider__item_active")
-    if (sliderNumber === 0) {
-        sliderNumber = sliderLength - 1
-    } else {
-        sliderNumber--
-    }
-
-    sliderItems.children[sliderNumber].classList.add("slider__item_active")
-}
+    sliderItems.children[sliderNumber].classList.remove("slider__item_active");
+    (sliderNumber === 0) ? sliderNumber = sliderLength - 1: sliderNumber--;
+    sliderItems.children[sliderNumber].classList.add("slider__item_active");
+};
