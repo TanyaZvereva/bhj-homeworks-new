3ab51117527a505a29a2ce6e462372af2f783ae5
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2020-08-20 21:18:07 +0300

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("task__input");
+const list = document.getElementById("tasks__list");
+const button = document.getElementById("tasks__add");
+
+function addTask() {
+    if (input.value !== "") {
+        list.innerHTML += `
+    <div class="task">
+    <div class="task__title">
+      ${input.value}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+  </div>
+`;
+        const forRemove = document.getElementsByClassName("task__remove");
+        Array.from(forRemove).forEach(element => {
+            element.onclick = (event) => {
+                element.closest(".task").remove();
+            };
+        });
+
+    };
+};
+button.onclick = (event) => {
+    event.preventDefault();
+    addTask();
+};
+input.keypress = (event) => {
+
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addTask();
+    };
+};
