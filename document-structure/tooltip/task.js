7bd0006c4f6a8c9beb3aaf43ae8f06aae66debbc
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2020-08-20 21:15:44 +0300

    first commit tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,17 @@
+const toolTipe = document.querySelector(".tooltip");
+const links = document.getElementsByClassName("has-tooltip");
+Array.from(links).forEach(link => {
+    link.onclick = () => {
+        const position = link.getBoundingClientRect();
+        const text = link.getAttribute("title");
+        if (toolTipe.style.display === "block") {
+            toolTipe.style.display = "none"
+        } else {
+            toolTipe.style.display = "block"
+        };
+        toolTipe.style.top = position.bottom + "px";
+        toolTipe.style.left = position.left + "px";
+        toolTipe.innerText = text;
+        return false;
+    };
+});
