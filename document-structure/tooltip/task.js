4bf7517bc13d768a907f256cedfbcf7a9640aa92
TanyaZvereva <61490384+TanyaZvereva@users.noreply.github.com>
2020-08-28 11:13:42 +0300

    third commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,11 +4,11 @@ Array.from(links).forEach(link => {
     link.onclick = () => {
         const position = link.getBoundingClientRect();
         const text = link.getAttribute("title");
-        // if (toolTipe.style.display === "block") {
-        //     toolTipe.style.display = "none"
-        // } else {
-        toolTipe.style.display = "block"
-        // };
+        if (toolTipe.style.display === "block") {
+            toolTipe.style.display = "none"
+        } else {
+            toolTipe.style.display = "block"
+        };
         toolTipe.style.top = position.bottom + "px";
         toolTipe.style.left = position.left + "px";
         toolTipe.innerText = text;
